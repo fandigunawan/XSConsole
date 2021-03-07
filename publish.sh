c79895c71bafdd378946db55d4b9895c4899227b
Fandi Gunawan <10239907+fandigunawan@users.noreply.github.com>
2021-03-07 16:24:05 +0700

    Fixes Linux path during travis-ci build

--- publish.sh
+++ publish.sh
@@ -5,12 +5,12 @@ dotnet publish -c Release -f netcoreapp3.1 --self-contained -o pub/Win32 -r win-
 dotnet publish -c Release -f netcoreapp3.1 --self-contained -o pub/Linux64 -r linux-x64 .
 dotnet publish -c Release -f netcoreapp3.1 --self-contained -o pub/LinuxMin64 -r linux-musl-x64 .
 dotnet publish -c Release -f netcoreapp3.1 --self-contained -o pub/OSX64 -r osx-x64 .
-cp xen-lic.txt xe/pub/Win64/
-cp xen-lic.txt xe/pub/Win32/
-cp xen-lic.txt xe/pub/Linux64/
-cp xen-lic.txt xe/pub/LinuxMin64/
-cp xen-lic.txt xe/pub/OSX64/
-cd xe/pub/
+cp xen-lic.txt pub/Win64/
+cp xen-lic.txt pub/Win32/
+cp xen-lic.txt pub/Linux64/
+cp xen-lic.txt pub/LinuxMin64/
+cp xen-lic.txt pub/OSX64/
+cd pub/
 zip -r XSConsole-Win64.zip Win64/
 zip -r XSConsole-Win32.zip Win32/
 zip -r XSConsole-Linux64.zip Linux64/
